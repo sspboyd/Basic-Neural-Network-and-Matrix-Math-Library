8bd410ea61133d2ef5963ca6fe8b1f5b4844ed45
sspboyd <sspboyd@gmail.com>
2018-11-02 23:40:25 -0400

    Added elementwise addition and multiplication.

--- matrix.js
+++ matrix.js
@@ -16,7 +16,7 @@ function Matrix(rows, cols) {
 }
 
 
-Matrix.prototype.randomize = function() {
+Matrix.prototype.randomize = function () {
     for (let i = 0; i < this.rows; i++) {
         for (let j = 0; j < this.cols; j++) {
             this.matrix[i][j] = Math.floor(Math.random() * 10);
@@ -25,19 +25,35 @@ Matrix.prototype.randomize = function() {
 }
 
 
-Matrix.prototype.multiply = function(n) {
-    for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-            this.matrix[i][j] *= n;
+Matrix.prototype.multiply = function (n) {
+    if (n instanceof Matrix) {
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                this.matrix[i][j] *= n.matrix[i][j];
+            }
+        }
+    } else {
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                this.matrix[i][j] *= n;
+            }
         }
     }
 }
 
 
-Matrix.prototype.add = function(n) {
-    for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-            this.matrix[i][j] += n;
+Matrix.prototype.add = function (n) {
+    if (n instanceof Matrix) {
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                this.matrix[i][j] += n.matrix[i][j];
+            }
+        }
+    } else {
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                this.matrix[i][j] += n;
+            }
         }
     }
 }
