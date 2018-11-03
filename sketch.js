3ca5a0b3204c80c607c1fc2e78ffd751fb745499
sspboyd <sspboyd@gmail.com>
2018-11-03 00:25:10 -0400

    Added testing code to see if the new dot product functionality is working (spoliers, it is!).

--- sketch.js
+++ sketch.js
@@ -3,6 +3,18 @@ let brain;
 function setup(){
 brain = new NeuralNetwork(3,3,1);
 
+let m = new Matrix(2,3);
+m.randomize();
+console.table(m.matrix);
+
+let n = new Matrix(3,2);
+n.randomize();
+console.table(n.matrix);
+
+let d = m.multiply(n);
+console.table(d.matrix);
+
+
 }
 
 function draw(){
