a55a0bdee8dddd18d3857bbcc11087b67d81b47a
sspboyd <sspboyd@gmail.com>
2018-11-08 16:17:45 -0500

    Updated code to help with implementing backpropagation.

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 function setup() {
-
-    let nn = new NeuralNetwork(2, 2, 1);
-    let input = [1, 0];
+    let nn = new NeuralNetwork(4, 4, 3);
+    let input = [1, 0, 0, 0];
+    targets = [1, 0, 1];
     let output = nn.feedfwd(input);
-    console.log(output);
+    // console.log(output);
+    nn.train(input, targets)
+    noLoop();
 }
 
 function draw() {
-
-
 }
